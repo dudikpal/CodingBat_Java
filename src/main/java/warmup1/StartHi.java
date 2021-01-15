a247fa77763cc664bf93471231a9a6c41c368959
dudikpal <dudikpal@gmail.com>
2021-01-15 21:41:13 +0100

    Warmup1 - StartHi class

---
+++ src/main/java/warmup1/StartHi.java
@@ -0,0 +1,10 @@
+package warmup1;
+
+public class StartHi {
+
+    public final String STARTWITH = "hi";
+
+    public boolean startHi(String str) {
+        return str.startsWith(STARTWITH);
+    }
+}
---
+++ src/test/java/warmup1/StartHiTest.java
@@ -0,0 +1,12 @@
+package warmup1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartHiTest {
+
+    @Test
+    void startHi() {
+    }
+}
