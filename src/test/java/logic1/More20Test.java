b368acc33ab6372de975cd3e86c9284ed1f4298b
dudikpal <dudikpal@gmail.com>
2021-01-17 22:43:53 +0100

    Logic-1 - More20 class

---
+++ src/test/java/logic1/More20Test.java
@@ -0,0 +1,15 @@
+package logic1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class More20Test {
+
+    @Test
+    void more20() {
+        int n = 22;
+
+        assertTrue(new More20().more20(n));
+    }
+}
