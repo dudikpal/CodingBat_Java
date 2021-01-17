9ba66405c9193f8709ccf7b8205ae9e3bbb2061e
dudikpal <dudikpal@gmail.com>
2021-01-17 18:01:51 +0100

    Logic-1 - SquirrelPlay class else ág törölve

--- src/main/java/logic1/SquirrelPlay.java
+++ src/main/java/logic1/SquirrelPlay.java
@@ -4,6 +4,7 @@ public class SquirrelPlay {
     public boolean squirrelPlay(int temp, boolean isSummer) {
         if (temp < 60 || temp > 100) {
             return false;
-        } else return isSummer || temp <= 90;
+        }
+        return isSummer || temp <= 90;
     }
 }
