904b091f4f6b6912a6c1e6f5d8a06babdd324024
dudikpal <dudikpal@gmail.com>
2021-01-17 22:46:34 +0100

    Logic-1 - Old35 class

---
+++ src/test/java/logic1/Old35Test.java
@@ -0,0 +1,15 @@
+package logic1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Old35Test {
+
+    @Test
+    void old35() {
+        int n = 15;
+
+        assertFalse(new Old35().old35(n));
+    }
+}
