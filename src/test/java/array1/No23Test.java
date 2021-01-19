cb075baabad831976c0bf90e1681ad3c8a03ce11
dudikpal <dudikpal@gmail.com>
2021-01-19 20:09:55 +0100

    Array-1 - No23 class

---
+++ src/test/java/array1/No23Test.java
@@ -0,0 +1,15 @@
+package array1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class No23Test {
+
+    @Test
+    void no23() {
+        int[] nums = {4, 5};
+
+        assertTrue(new No23().no23(nums));
+    }
+}
