60c6c43e907e14ecbe3f71b69db475ea005dd3cd
dudikpal <dudikpal@gmail.com>
2021-03-13 00:16:05 +0100

    Recursion-1 BunnyEars class deleted bunnies == 1 "if" statement

--- src/main/java/recursion/BunnyEars.java
+++ src/main/java/recursion/BunnyEars.java
@@ -3,12 +3,25 @@ package recursion;
 public class BunnyEars {
     
     public int bunnyEars(int bunnies) {
-        if (bunnies == 1) {
-            return 2;
+        if (bunnies == 0) {
+            return 0;
         }
         return 2 + bunnyEars(bunnies - 1);
     }
 }
+
+/*
+Our Solution:
+
+public int bunnyEars(int bunnies) {
+  // Base case: if bunnies==0, just return 0.
+  if (bunnies == 0) return 0;
+  
+  // Recursive case: otherwise, make a recursive call with bunnies-1
+  // (towards the base case), and fix up what it returns.
+  return 2 + bunnyEars(bunnies-1);
+}
+ */
 
 /*
 We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
