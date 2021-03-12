aae65732b55f38d6ae18a699426153e296a0802e
dudikpal <dudikpal@gmail.com>
2021-03-13 00:39:03 +0100

    Recursion-1 BunnyEars2 class

---
+++ src/test/java/recursion/BunnyEars2Test.java
@@ -0,0 +1,15 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BunnyEars2Test {
+    
+    @Test
+    void bunnyEars2() {
+        
+        assertEquals(10, new BunnyEars2().bunnyEars2(4));
+        
+    }
+}
