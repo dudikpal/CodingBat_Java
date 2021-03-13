90dfa6d8740418c071196df4aa502daf3931b688
dudikpal <dudikpal@gmail.com>
2021-03-13 16:36:30 +0100

    Recursion-1 CountX class

---
+++ src/test/java/recursion/CountXTest.java
@@ -0,0 +1,15 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountXTest {
+    
+    @Test
+    void countX() {
+        
+        assertEquals(4, new CountX().countX("xxhixx"));
+        
+    }
+}
