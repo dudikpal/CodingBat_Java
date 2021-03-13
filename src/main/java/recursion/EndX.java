451d375368e095b6d0d01d078e3840fb959454ce
dudikpal <dudikpal@gmail.com>
2021-03-13 21:45:06 +0100

    Recursion-1 EndX class

---
+++ src/main/java/recursion/EndX.java
@@ -0,0 +1,23 @@
+package recursion;
+
+public class EndX {
+    
+    public String endX(String str) {
+    
+        if (str.length() == 0) {
+            return str;
+        }
+        if (str.startsWith("x")) {
+            return endX(str.substring(1)) + "x";
+        }
+        return str.charAt(0) + endX(str.substring(1));
+    }
+}
+
+/*
+Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of the string.
+
+endX("xxre") → "rexx"
+endX("xxhixx") → "hixxxx"
+endX("xhixhix") → "hihixxx"
+ */
---
+++ src/test/java/recursion/EndXTest.java
@@ -0,0 +1,15 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EndXTest {
+    
+    @Test
+    void endX() {
+        
+        assertEquals("hihixxx", new EndX().endX("xhixhix"));
+        
+    }
+}
