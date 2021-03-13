2d68dc943aa74aec491f44fb03a46ed04a28d94b
dudikpal <dudikpal@gmail.com>
2021-03-13 16:11:49 +0100

    Recursion-1 Count7 class

---
+++ src/test/java/recursion/Count7Test.java
@@ -0,0 +1,15 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Count7Test {
+    
+    @Test
+    void count7() {
+        
+        assertEquals(2, new Count7().count7(717));
+        
+    }
+}
