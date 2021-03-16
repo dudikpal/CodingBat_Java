d6246ddaeaaa81e213cca469876959f636f24cf2
dudikpal <dudikpal@gmail.com>
2021-03-16 13:13:40 +0100

    Warmup2 - ArrayCount9 class

---
+++ src/main/java/warmup2/arraycount9/ArrayCount9.java
@@ -0,0 +1,15 @@
+package warmup2.arraycount9;
+
+public class ArrayCount9 {
+    
+    public int arrayCount9(int[] nums) {
+        
+        int counter = 0;
+        for (int num : nums) {
+            if (num == 9) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/warmup2/arraycount9/ArrayCount9Test.java
@@ -0,0 +1,15 @@
+package warmup2.arraycount9;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayCount9Test {
+    
+    @Test
+    void arrayCount9() {
+        
+        assertEquals(3, new ArrayCount9().arrayCount9(new int[]{1, 9, 7, 9, 9, 8}));
+        
+    }
+}
