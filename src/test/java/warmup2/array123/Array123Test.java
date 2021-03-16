b0c6f9cdf1b0cd93696b6666be022b1c9ded45ee
dudikpal <dudikpal@gmail.com>
2021-03-16 13:52:48 +0100

    Warmup2 - Array123 class

---
+++ src/test/java/warmup2/array123/Array123Test.java
@@ -0,0 +1,15 @@
+package warmup2.array123;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Array123Test {
+    
+    @Test
+    void array123() {
+        
+        assertTrue(new Array123().array123(new int[]{1, 1, 2, 3, 4}));
+        
+    }
+}
