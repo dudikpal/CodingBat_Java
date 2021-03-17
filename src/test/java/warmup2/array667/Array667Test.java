dcfa86334e5606914b6d8635411806cafb3b1089
dudikpal <dudikpal@gmail.com>
2021-03-17 21:59:07 +0100

    Warmup2 - Array667 class

---
+++ src/test/java/warmup2/array667/Array667Test.java
@@ -0,0 +1,15 @@
+package warmup2.array667;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Array667Test {
+    
+    @Test
+    void array667() {
+    
+        assertEquals(1, new Array667().array667(new int[]{6, 7, 2, 6}));
+    
+    }
+}
