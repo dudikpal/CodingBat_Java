4cdf82f0321826546c553e8c8c4e368dce1c2fb8
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-06-02 23:37:16 +0200

    #2: Array - 2 - TenRun refactor the algorithm

--- src/main/java/array2/tenrun/TenRun.java
+++ src/main/java/array2/tenrun/TenRun.java
@@ -3,6 +3,20 @@ package array2.tenrun;
 public class TenRun {
 
     public int[] tenRun(int[] nums) {
+        int actual = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 10 == 0 && nums[i] != actual) {
+                actual = nums[i];
+            }
+            if (actual != -1) {
+                nums[i] = actual;
+            }
+        }
+        return nums;
+    }
+
+
+    /*public int[] tenRun(int[] nums) {
         int actual = 0;
         boolean wasTenTimes = false;
         for (int i = 0; i < nums.length; i++) {
@@ -14,9 +28,8 @@ public class TenRun {
             if (wasTenTimes) {
                 nums[i] = actual;
             }
-
         }
         return nums;
-    }
+    }*/
 
 }
