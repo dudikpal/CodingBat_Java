e8ecf306dcfad12ab709b181a3aeee377d3c9f14
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-08-30 11:38:57 +0200

    #2: Map - 2 - FirstChar refactor with only stream

--- src/main/java/map2/firstchar/FirstChar.java
+++ src/main/java/map2/firstchar/FirstChar.java
@@ -1,19 +1,32 @@
 package map2.firstchar;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 public class FirstChar {
 
     public Map<String, String> firstChar(String[] strings) {
 
-        Map<String, String> result = new HashMap<>();
-        for (String word : strings) {
-            result.merge(word.substring(0, 1), word, (prev, next) -> prev + next);
-        }
-        return result;
+        //Map<String, String> result = new HashMap<>();
+        /*for (String word : strings) {
+            result.merge(word.substring(0, 1), word, String::concat);
+        }*/
+
+        /*Arrays.stream(strings)
+                .forEach(word -> result.merge(word.substring(0, 1), word, String::concat));
+        return result;*/
+
+        return Arrays.stream(strings)
+                .collect(
+                        Collectors.toMap(word -> word.substring(0, 1),
+                                Function.identity(),
+                                String::concat)
+                );
     }
 
 }
